d495b7042e9086fa32a8a55e0a6c370e0ea7c1a6
pavel <paveltessman@gmail.com>
2024-09-03 22:01:34 +0300

    Tried to add bind for Ex (does not work, so commented out)

--- nvim/lua/vim-keymaps.lua
+++ nvim/lua/vim-keymaps.lua
@@ -58,6 +58,7 @@ vim.o.background = "dark" -- or "light" for light mode
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- vim.keymap.set('n', '<leader>fe', vim.cmd('Ex'), {desc = 'Open dir view'})
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
