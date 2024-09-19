3cb54ea88c611fa8f2a59b94a774f889dac91eda
pavel <paveltessman@gmail.com>
2024-09-19 20:47:10 +0300

    Disable oil icons

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,7 +7,11 @@ return {
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
-      require("oil").setup()
+      require("oil").setup({
+
+      columns = {}
+
+    })
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end
 }
